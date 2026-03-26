9c1ae852a09b326a21b6bd40d22ce9e905982a32
Thiago Honorato <thiagohbhonorato@gmail.com>
2026-03-26 08:26:31 -0300

    Estrutura inicial

---
+++ src/main/java/com/empresa/honorato/HonoratoApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.honorato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonoratoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonoratoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresa/honorato/HonoratoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.honorato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HonoratoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
